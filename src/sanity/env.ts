56198f9d2a886f36403f254480ffba52ec570b36
andrew <113289014+aagb1884@users.noreply.github.com>
2024-12-14 21:06:42 +0000

    update sanity schema

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -3,7 +3,7 @@ export const apiVersion =
 
 export const dataset = assertValue(
   process.env.SANITY_STUDIO_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
+  'Missing environment variable: SANITY_STUDIO_DATASET'
 )
 
 export const projectId = assertValue(
