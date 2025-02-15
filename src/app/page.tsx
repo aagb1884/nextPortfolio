8ae180f2b0861a526c3c1d74cc9fef7b1ec659e0
andrew <113289014+aagb1884@users.noreply.github.com>
2025-02-15 16:44:34 +0000

    removed unused link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import Link from "next/link";
 import styles from "./styles/page.module.css";
 import { useRef } from "react";
 import HeaderLinks from '../components/HomePage/Header/HeaderLinks';
