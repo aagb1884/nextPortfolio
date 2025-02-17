bb2f1d464ad73bb13b57a8f83970c66365773493
andrew <113289014+aagb1884@users.noreply.github.com>
2025-02-17 12:29:02 +0000

    removed apostrophe - are you happy now es lint?

--- src/app/apps/barlow/components/demo.tsx
+++ src/app/apps/barlow/components/demo.tsx
@@ -8,7 +8,7 @@ interface modalProps {
     return (
         <div className={styles.startModalWrapper}>  
         <div className={styles.startModal}>   
-        <h2 className={styles.sModalTxt}>What's in the full version?</h2>
+        <h2 className={styles.sModalTxt}>What&apos;s in the full version?</h2>
         <p className={styles.sModalTxt}>You get 60 seconds per <i>guess</i>, not 60 seconds per game.</p>
         <p className={styles.sModalTxt}>Works offline.</p>
         <p className={styles.sModalTxt}>High Scores.</p>
