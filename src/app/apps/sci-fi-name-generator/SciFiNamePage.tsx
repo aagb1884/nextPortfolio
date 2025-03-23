8484da4d9efb40c9c5c0323460996890ebc288a3
andrew <113289014+aagb1884@users.noreply.github.com>
2025-03-23 10:10:57 +0000

    chibnall filter added

--- src/app/apps/sci-fi-name-generator/SciFiNamePage.tsx
+++ src/app/apps/sci-fi-name-generator/SciFiNamePage.tsx
@@ -20,13 +20,32 @@ interface JsonData{
 const SciFiNamePage: React.FC<JsonData> = ({syllables}) => {
     const [name, setName] = useState<string>("");
     const [noOfSyllables, setNumberOfSyllables] = useState(1);
+    const [chibnallFilter, setChibnallFilter] = useState(false);
 
     function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     setNumberOfSyllables(e.target.valueAsNumber);
     } 
 
+    const chibnallFilteredStrings = (array: string[]) => {
+        return array.filter((syllable) => syllable.includes("k"))
+    } 
+
     function getRandomName(number: number) {
-    if (number === 1) {
+    const chibnallOne = chibnallFilteredStrings(syllables.single)
+    const chibnallLong = chibnallFilteredStrings(syllables.syllables)
+    if (chibnallFilter && number === 1) {
+        const randomElement = chibnallOne[Math.floor(Math.random() * chibnallOne.length)];
+        setName(randomElement)
+    }
+    else if (chibnallFilter && number > 1) {
+        const randomElement = chibnallOne[Math.floor(Math.random() * chibnallOne.length)];
+        const scrambled = chibnallLong?.sort((a,b) => 0.5 - Math.random());
+        const noSyllablesRequired = noOfSyllables - 1
+        const getSyllables = scrambled?.slice(0, noSyllablesRequired);
+        const joinSyllables = getSyllables.join("").toLowerCase()
+        setName(randomElement + joinSyllables)
+    }
+    else if (number === 1) {
         const randomElement = syllables.single[Math.floor(Math.random() * syllables.single.length)];
         setName(randomElement)
     }
@@ -61,6 +80,7 @@ const SciFiNamePage: React.FC<JsonData> = ({syllables}) => {
             <h1 className={styles.sciFiTitle}>Sci Fi Character Name Generator</h1>
             <div className={styles.noOfSyllablesDiv}>
             Number of Syllables:
+
             <input 
             value={noOfSyllables}
             onChange={handleChange}
@@ -70,6 +90,9 @@ const SciFiNamePage: React.FC<JsonData> = ({syllables}) => {
             name='noOfSyllables'
             className={styles.noOfSyllablesInput}
             />
+            <button onClick={() => {setChibnallFilter(!chibnallFilter)}}>
+                Chibnall Filter
+            </button>
             </div>
             <button
             onClick={() => {getRandomName(noOfSyllables)}}
