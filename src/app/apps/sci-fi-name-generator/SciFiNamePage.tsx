92641266b46ce6f462fb5c7fc807a73f57ab1d66
aagb1884 <a.a.g.blair@gmail.com>
2025-03-24 10:02:01 +0000

    updating functions@

--- src/app/apps/sci-fi-name-generator/SciFiNamePage.tsx
+++ src/app/apps/sci-fi-name-generator/SciFiNamePage.tsx
@@ -1,123 +1,118 @@
-'use client'
+"use client";
 
 import styles from "@/app/styles/sciFiName.module.css";
-import {useState} from 'react';
+import { useState } from "react";
 import Image from "next/image";
 import AppsFooter from "../components/AppsFooter";
 
-interface JsonData{
-    syllables: {
-      single: [
-          syllable: string
-      ],
-      syllables: [
-        syllable: string
-      ]
-      }
-  }
-  
+interface JsonData {
+  syllables: {
+    single: [syllable: string];
+    syllables: [syllable: string];
+  };
+}
 
-const SciFiNamePage: React.FC<JsonData> = ({syllables}) => {
-    const [name, setName] = useState<string>("");
-    const [noOfSyllables, setNumberOfSyllables] = useState(1);
-    const [chibnallFilter, setChibnallFilter] = useState(false);
+const SciFiNamePage: React.FC<JsonData> = ({ syllables }) => {
+  const [name, setName] = useState<string>("");
+  const [noOfSyllables, setNumberOfSyllables] = useState(1);
+  const [chibnallFilter, setChibnallFilter] = useState(false);
 
-    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     setNumberOfSyllables(e.target.valueAsNumber);
-    } 
+  }
 
-    const chibnallFilteredStrings = (array: string[]) => {
-        return array.filter((syllable) => syllable.includes("k"))
-    } 
+  const chibnallFilteredStrings = (array: string[]) => {
+    return array.filter((syllable) => syllable.includes("k"));
+  };
 
-    function getRandomName(number: number) {
-    const chibnallOne = chibnallFilteredStrings(syllables.single)
-    const chibnallLong = chibnallFilteredStrings(syllables.syllables)
+  const getRandomFirstSyllable = (array: string[]) => {
+    return array[Math.floor(Math.random() * array.length)];
+  };
+
+  function getRandomName(number: number) {
+    const chibnallOne = chibnallFilteredStrings(syllables.single);
+    const chibnallLong = chibnallFilteredStrings(syllables.syllables);
     if (chibnallFilter && number === 1) {
-        const randomElement = chibnallOne[Math.floor(Math.random() * chibnallOne.length)];
-        setName(randomElement)
+      const randomElement = getRandomFirstSyllable(chibnallOne);
+      setName(randomElement);
+    } else if (chibnallFilter && number > 1) {
+      const randomElement = getRandomFirstSyllable(chibnallOne);
+      const scrambled = chibnallLong?.sort(() => 0.5 - Math.random());
+      const noSyllablesRequired = noOfSyllables - 1;
+      const getSyllables = scrambled?.slice(0, noSyllablesRequired);
+      const joinSyllables = getSyllables.join("").toLowerCase();
+      setName(randomElement + joinSyllables);
+    } else if (number === 1) {
+      const randomElement = getRandomFirstSyllable(syllables.single);
+      setName(randomElement);
+    } else if (number > 1) {
+      const randomElement =
+        syllables.single[Math.floor(Math.random() * syllables.single.length)];
+      const scrambled = syllables.syllables?.sort(() => 0.5 - Math.random());
+      const noSyllablesRequired = noOfSyllables - 1;
+      const getSyllables = scrambled?.slice(0, noSyllablesRequired);
+      const joinSyllables = getSyllables.join("").toLowerCase();
+      setName(randomElement + joinSyllables);
     }
-    else if (chibnallFilter && number > 1) {
-        const randomElement = chibnallOne[Math.floor(Math.random() * chibnallOne.length)];
-        const scrambled = chibnallLong?.sort((a,b) => 0.5 - Math.random());
-        const noSyllablesRequired = noOfSyllables - 1
-        const getSyllables = scrambled?.slice(0, noSyllablesRequired);
-        const joinSyllables = getSyllables.join("").toLowerCase()
-        setName(randomElement + joinSyllables)
+  }
+
+  const handleCopyClick = async () => {
+    try {
+      await window.navigator.clipboard.writeText(name);
+      alert("Copied to clipboard!");
+    } catch (err) {
+      console.error("Unable to copy to clipboard.", err);
+      alert("Copy to clipboard failed.");
     }
-    else if (number === 1) {
-        const randomElement = syllables.single[Math.floor(Math.random() * syllables.single.length)];
-        setName(randomElement)
-    }
-    else if (number > 1) {
-        const randomElement = syllables.single[Math.floor(Math.random() * syllables.single.length)];
-        const scrambled = syllables.syllables?.sort((a,b) => 0.5 - Math.random());
-        const noSyllablesRequired = noOfSyllables - 1
-        const getSyllables = scrambled?.slice(0, noSyllablesRequired);
-        const joinSyllables = getSyllables.join("").toLowerCase()
-        setName(randomElement + joinSyllables)
-    }
+  };
 
-    }
-
-    const handleCopyClick = async () => {
-        try {
-            await window.navigator.clipboard.writeText(name);
-            alert("Copied to clipboard!");
-        } catch (err) {
-            console.error(
-                "Unable to copy to clipboard.",
-                err
-            );
-            alert("Copy to clipboard failed.");
-        }
-    };
-
-
-    return ( 
-        <main className={styles.sciFiNameMain}>
-            <AppsFooter />
-            <h1 className={styles.sciFiTitle}>Sci Fi Character Name Generator</h1>
-            <div className={styles.noOfSyllablesDiv}>
-            Number of Syllables:
-
-            <input 
-            value={noOfSyllables}
-            onChange={handleChange}
-            type='number'
-            min={1}
-            max={10}
-            name='noOfSyllables'
-            className={styles.noOfSyllablesInput}
-            />
-            <button onClick={() => {setChibnallFilter(!chibnallFilter)}}>
-                Chibnall Filter
-            </button>
-            </div>
-            <button
-            onClick={() => {getRandomName(noOfSyllables)}}
-            className={styles.getNameBtn}>
-                Get Sci Fi Name
-            </button>
-            <div className={styles.name}>
-                {name}
-            </div>
-            <div className={styles.copyClipboard}>
-            <button
-            onClick={handleCopyClick}
-            >
-            <Image
+  return (
+    <main className={styles.sciFiNameMain}>
+      <AppsFooter />
+      <h1 className={styles.sciFiTitle}>Sci Fi Character Name Generator</h1>
+      <button>How Does This Work?</button>
+      <div className={styles.noOfSyllablesDiv}>
+        Number of Syllables:
+        <input
+          value={noOfSyllables}
+          onChange={handleChange}
+          type="number"
+          min={1}
+          max={10}
+          name="noOfSyllables"
+          className={styles.noOfSyllablesInput}
+        />
+        <button
+          onClick={() => {
+            setChibnallFilter(!chibnallFilter);
+          }}
+        >
+          Chibnall Filter
+        </button>
+      </div>
+      <button
+        onClick={() => {
+          getRandomName(noOfSyllables);
+        }}
+        className={styles.getNameBtn}
+      >
+        Get Sci Fi Name
+      </button>
+      <div className={styles.name}>{name}</div>
+      <div className={styles.copyClipboard}>
+        <button onClick={handleCopyClick}>
+          <Image
             src="/images/icons8-copy-to-clipboard-48.png"
             alt="Copy to Clipboard icon by Icons8"
             title="Copy to Clipboard (icon by Icons8)"
             id="clipboard"
             width={30}
             height={30}
-            />
-            </button>
-        </div>
-        </main>
-     );
-}
- 
+          />
+        </button>
+      </div>
+    </main>
+  );
+};
+
 export default SciFiNamePage;
