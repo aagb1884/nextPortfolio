94dee51cc83a12d554a8d87697eaa75727554829
aagb1884 <a.a.g.blair@gmail.com>
2026-01-14 19:04:01 +0000

    updated barlow and sitemap

--- src/app/apps/barlow/components/demo.tsx
+++ src/app/apps/barlow/components/demo.tsx
@@ -1,29 +1,29 @@
 interface modalProps {
-    setDemoModal: (filterTerm: boolean) => void;
-  }
-  
-  import styles from '@/app/styles/barlow.module.css';
-  
-  const DemoModal: React.FC<modalProps> = ({setDemoModal}) => {
-    return (
-        <div className={styles.startModalWrapper}>  
-        <div className={styles.startModal}>   
+  setDemoModal: (filterTerm: boolean) => void;
+}
+
+import styles from "@/app/styles/barlow.module.css";
+
+const DemoModal: React.FC<modalProps> = ({ setDemoModal }) => {
+  return (
+    <div className={styles.startModalWrapper}>
+      <div className={styles.startModal}>
         <h2 className={styles.sModalTxt}>What&apos;s in the full version?</h2>
-        <p className={styles.sModalTxt}>You get 60 seconds per <i>guess</i>, not 60 seconds per game.</p>
+        <p className={styles.sModalTxt}>
+          You get 60 seconds per <i>guess</i>, not 60 seconds per game.
+        </p>
         <p className={styles.sModalTxt}>Works offline.</p>
         <p className={styles.sModalTxt}>High Scores.</p>
-        <p className={styles.sModalTxt}>Original music with inbuilt mute option.</p>
-        <button className={styles.barlowBtn}
-        onClick={() => setDemoModal(false)}>
-        Close</button>
-        </div>
-        </div>
-    
-     
-        
-       
-        
-    )
+        <p className={styles.sModalTxt}>Original music.</p>
+        <button
+          className={styles.barlowBtn}
+          onClick={() => setDemoModal(false)}
+        >
+          Close
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default DemoModal;
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,48 +1,84 @@
-import type { MetadataRoute } from 'next';
+import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://andrewblair.co.uk',
+      url: "https://andrewblair.co.uk",
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: "yearly",
       priority: 1,
-      images: ['https://andrewblair.co.uk/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar_ab.d7ee2f77.png&amp;w=256&amp;q=75']
+      images: [
+        "https://andrewblair.co.uk/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar_ab.d7ee2f77.png&amp;w=256&amp;q=75",
+      ],
     },
     {
-      url: 'https://andrewblair.co.uk/blog',
+      url: "https://andrewblair.co.uk/blog",
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "monthly",
       priority: 0.9,
-      images: ['https://andrewblair.co.uk/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar_ab.d7ee2f77.png&amp;w=256&amp;q=75']
+      images: [
+        "https://andrewblair.co.uk/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar_ab.d7ee2f77.png&amp;w=256&amp;q=75",
+      ],
     },
     {
-      url: 'https://andrewblair.co.uk/apps',
+      url: "https://andrewblair.co.uk/apps",
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: "yearly",
       priority: 0.8,
-      images: ['https://andrewblair.co.uk/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar_ab.d7ee2f77.png&amp;w=256&amp;q=75']
+      images: [
+        "https://andrewblair.co.uk/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar_ab.d7ee2f77.png&amp;w=256&amp;q=75",
+      ],
     },
     {
-      url: 'https://andrewblair.co.uk/apps/eurovision-bingo',
+      url: "https://andrewblair.co.uk/apps/eurovision-bingo",
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: "yearly",
       priority: 0.7,
-      images: ['https://andrewblair.co.uk/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Feurovision_bingo_button.f490ee4e.gif&amp;w=640&amp;q=75']
+      images: [
+        "https://andrewblair.co.uk/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Feurovision_bingo_button.f490ee4e.gif&amp;w=640&amp;q=75",
+      ],
     },
     {
-      url: 'https://andrewblair.co.uk/apps/big-finish-generator',
+      url: "https://andrewblair.co.uk/apps/big-finish-generator",
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: "yearly",
       priority: 0.7,
-      images: ['https://andrewblair.co.uk/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Funtempered_schism.7214938d.gif&amp;w=1080&amp;q=75']
+      images: [
+        "https://andrewblair.co.uk/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Funtempered_schism.7214938d.gif&amp;w=1080&amp;q=75",
+      ],
     },
     {
-      url: 'https://andrewblair.co.uk/credits',
+      url: "https://andrewblair.co.uk/apps/sci-fi-name-generator",
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: "yearly",
+      priority: 0.7,
+    },
+    {
+      url: "https://andrewblair.co.uk/apps/light",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.7,
+    },
+    {
+      url: "https://andrewblair.co.uk/apps/drwhoquiz",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.7,
+    },
+    {
+      url: "https://andrewblair.co.uk/apps/poetremixer",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.7,
+    },
+    {
+      url: "https://andrewblair.co.uk/credits",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
       priority: 0.5,
-      images: ['https://andrewblair.co.uk/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar_ab.d7ee2f77.png&amp;w=256&amp;q=75']
-    }
+      images: [
+        "https://andrewblair.co.uk/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Favatar_ab.d7ee2f77.png&amp;w=256&amp;q=75",
+      ],
+    },
   ];
 }
