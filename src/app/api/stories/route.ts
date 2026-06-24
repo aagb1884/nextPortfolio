e6b8edcd840f2a1d4074113a70fb83dcb1d358a8
aagb1884 <a.a.g.blair@gmail.com>
2026-06-24 10:22:01 +0100

    updated route.ts

--- src/app/api/stories/route.ts
+++ src/app/api/stories/route.ts
@@ -1,6 +1,6 @@
 import { stories } from "@/app/apps/light/data/stories";
 
-export async function GET(request: Request) {
+export async function GET() {
   // For example, fetch data from your DB here
   return new Response(JSON.stringify(stories), {
     status: 200,
