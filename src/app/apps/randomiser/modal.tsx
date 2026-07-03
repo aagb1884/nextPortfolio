3c69f89bf090270710f25cb7bfa18acccbc3ff3a
aagb1884 <a.a.g.blair@gmail.com>
2026-07-03 19:54:41 +0100

    added links

--- src/app/apps/randomiser/modal.tsx
+++ src/app/apps/randomiser/modal.tsx
@@ -5,6 +5,7 @@ interface modalProps {
 
 import styles from "@/app/styles/randomiser.module.css";
 import KoFiLink from "@/app/ui/KoFi";
+import Link from "next/link";
 
 const StartModal: React.FC<modalProps> = ({ setStartModal, playHum }) => {
   const begin = () => {
@@ -16,21 +17,52 @@ const StartModal: React.FC<modalProps> = ({ setStartModal, playHum }) => {
     <div className={styles.startModalWrapper}>
       <div className={styles.startModal}>
         <h1 className={styles.modalTitle}>THE RANDOMISER</h1>
-        <h2 className={styles.sModalTxt}>How to Use</h2>
-        <p className={styles.sModalTxt}>
+        <h2 className={styles.sModalTxt}>
           If you aren&apos;t sure what Doctor Who story to watch...
-        </p>
+        </h2>
         <p className={styles.sModalTxt}>
-          There&apos;s a red lever on the TARDIS console.
+          There&apos;s a red lever on the TARDIS console. If you pull it,
+          you&apos;ll get a random Doctor Who story.
         </p>
-        <p className={styles.sModalTxt}>
-          If you pull it, you&apos;ll get a random Doctor Who story.
-        </p>
-
-        <p className={styles.sModalTxt}>You can watch it if you want.</p>
-        <p className={styles.sModalTxt}>
-          If you&apos;re in the UK, most Doctor Who stories are on BBC I-Player.
-        </p>
+        <p className={styles.sModalTxt}>You can then watch it if you want.</p>
+        <ul>
+          <li className={styles.sModalLi}>
+            UK: most Doctor Who stories are on{" "}
+            <Link href="https://www.bbc.co.uk/iplayer" target="_blank">
+              BBC I-Player
+            </Link>
+            .
+          </li>
+          <li className={styles.sModalLi}>
+            America: most 1963 - 1996 Doctor Who stories are on{" "}
+            <Link href="https://www.britbox.com/" target="_blank">
+              Britbox
+            </Link>
+            , 2005 - 2022 stories are on{" "}
+            <Link href="https://www.amcplus.com/" target="_blank">
+              AMC
+            </Link>
+            , and 2023 - 2025 stories are on
+            <Link href="https://www.disneyplus.com/" target="_blank">
+              Disney +
+            </Link>
+            .
+          </li>
+          <li className={styles.sModalLi}>
+            Australia: most 1963 - 1996 Doctor Who stories are on{" "}
+            <Link href="https://www.britbox.com/" target="_blank">
+              Britbox
+            </Link>{" "}
+            and 2023 - 2025 stories are on{" "}
+            <Link href="https://www.disneyplus.com/" target="_blank">
+              Disney +
+            </Link>
+            .
+          </li>
+          <li className={styles.sModalLi}>
+            The show is also available on physical media.
+          </li>
+        </ul>
         <br />
         <p className={styles.sModalTxt}>
           Please consider donating to help me cover my costs.
@@ -38,7 +70,7 @@ const StartModal: React.FC<modalProps> = ({ setStartModal, playHum }) => {
         <p className={styles.sModalStr}>↓↓</p>
         <KoFiLink />
         <button className={styles.startBtn} onClick={begin}>
-          Let's Get Random
+          Let&apos;s Get Random
         </button>
       </div>
     </div>
