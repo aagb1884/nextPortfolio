d085312a4ca0314507bd6139a14c9e9a7349172f
aagb1884 <a.a.g.blair@gmail.com>
2026-07-23 21:29:30 +0100

    added detail to right of console

--- src/app/apps/randomiser/components/console/Right.tsx
+++ src/app/apps/randomiser/components/console/Right.tsx
@@ -1,7 +1,12 @@
 import styles from "@/app/styles/randomiser.module.css";
 
 const Right = () => {
-  return <div className={styles.consoleRight} />;
+  return (
+    <div className={styles.consoleRight}>
+      <div className={styles.telepath} />
+      <div className={styles.grid} />
+    </div>
+  );
 };
 
 export default Right;
