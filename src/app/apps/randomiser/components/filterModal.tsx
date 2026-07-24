dc0bccac89369f077efc0a0b474a41ddcc55f52c
aagb1884 <a.a.g.blair@gmail.com>
2026-07-24 17:57:43 +0100

    update modals layout

--- src/app/apps/randomiser/components/filterModal.tsx
+++ src/app/apps/randomiser/components/filterModal.tsx
@@ -39,105 +39,110 @@ const FilterModal: React.FC<modalProps> = ({
         <p className={styles.fModalTxt}>
           NB. Some filters may result in spoilers.
         </p>
-        <div className={styles.doctorSelect}>
-          <label htmlFor="doctors">Select Doctor(s):</label>
-          <select
-            className={styles.multiSelect}
-            name="doctors"
-            id="doctors"
-            multiple
-            value={filterDoctors}
-            onChange={(e) => {
-              const values = Array.from(
-                e.target.selectedOptions,
-                (option) => option.value
-              );
-              setFilterDoctors(values);
-            }}
-          >
-            <option className={styles.option} value="First">
-              First
-            </option>
-            <option className={styles.option} value="Second">
-              Second
-            </option>
-            <option className={styles.option} value="Third">
-              Third
-            </option>
-            <option className={styles.option} value="Fourth">
-              Fourth
-            </option>
-            <option className={styles.option} value="Fifth">
-              Fifth
-            </option>
-            <option className={styles.option} value="Sixth">
-              Sixth
-            </option>
-            <option className={styles.option} value="Seventh">
-              Seventh
-            </option>
-            <option className={styles.option} value="Eighth">
-              Eighth
-            </option>
-            <option className={styles.option} value="Ninth">
-              Ninth
-            </option>
-            <option className={styles.option} value="Tenth">
-              Tenth
-            </option>
-            <option className={styles.option} value="Eleventh">
-              Eleventh
-            </option>
-            <option className={styles.option} value="Twelfth">
-              Twelfth
-            </option>
-            <option className={styles.option} value="Thirteenth">
-              Thirteenth
-            </option>
-            <option className={styles.option} value="Fourteenth">
-              Fourteenth
-            </option>
-            <option className={styles.option} value="Fifteenth">
-              Fifteenth
-            </option>
-          </select>
+        <div className={styles.filters}>
+          <div className={styles.itemSelect}>
+            <label htmlFor="doctors">Select Doctor(s):</label>
+            <select
+              className={styles.multiSelect}
+              name="doctors"
+              id="doctors"
+              multiple
+              value={filterDoctors}
+              onChange={(e) => {
+                const values = Array.from(
+                  e.target.selectedOptions,
+                  (option) => option.value
+                );
+                setFilterDoctors(values);
+              }}
+            >
+              <option className={styles.option} value="First">
+                First
+              </option>
+              <option className={styles.option} value="Second">
+                Second
+              </option>
+              <option className={styles.option} value="Third">
+                Third
+              </option>
+              <option className={styles.option} value="Fourth">
+                Fourth
+              </option>
+              <option className={styles.option} value="Fifth">
+                Fifth
+              </option>
+              <option className={styles.option} value="Sixth">
+                Sixth
+              </option>
+              <option className={styles.option} value="Seventh">
+                Seventh
+              </option>
+              <option className={styles.option} value="Eighth">
+                Eighth
+              </option>
+              <option className={styles.option} value="Ninth">
+                Ninth
+              </option>
+              <option className={styles.option} value="Tenth">
+                Tenth
+              </option>
+              <option className={styles.option} value="Eleventh">
+                Eleventh
+              </option>
+              <option className={styles.option} value="Twelfth">
+                Twelfth
+              </option>
+              <option className={styles.option} value="Thirteenth">
+                Thirteenth
+              </option>
+              <option className={styles.option} value="Fourteenth">
+                Fourteenth
+              </option>
+              <option className={styles.option} value="Fifteenth">
+                Fifteenth
+              </option>
+            </select>
+          </div>
           <br />
-          <label htmlFor="eras">Select Era(s):</label>
-          <select
-            className={styles.eraSelect}
-            name="eras"
-            id="eras"
-            multiple={true}
-            value={filterEras}
-            onChange={(e) => {
-              const values = Array.from(
-                e.target.selectedOptions,
-                (option) => option.value
-              );
-              setFilterEras(values);
-            }}
-          >
-            <option className={styles.option} value="1963 - 1996">
-              1963 - 1996
-            </option>
-            <option className={styles.option} value="2005 - Present Day">
-              2005 - Present Day
-            </option>
-          </select>
+          <div className={styles.itemSelect}>
+            <label htmlFor="eras">Select Era(s):</label>
+            <select
+              className={styles.eraSelect}
+              name="eras"
+              id="eras"
+              multiple={true}
+              value={filterEras}
+              onChange={(e) => {
+                const values = Array.from(
+                  e.target.selectedOptions,
+                  (option) => option.value
+                );
+                setFilterEras(values);
+              }}
+            >
+              <option className={styles.option} value="1963 - 1996">
+                1963 - 1996
+              </option>
+              <option className={styles.option} value="2005 - Present Day">
+                2005 - Present Day
+              </option>
+            </select>
+          </div>
           <br />
-          <label htmlFor="freetype">Filter by Text:</label>
+          <div className={styles.itemSelect}>
+            <label htmlFor="freetype">Filter by Text:</label>
 
-          <input
-            type="text"
-            id="freetype"
-            name="freetype"
-            className={styles.filterInput}
-            value={filterTerm}
-            onChange={(e) => {
-              setFilterTerm(e.target.value);
-            }}
-          />
-
+            <input
+              type="text"
+              id="freetype"
+              name="freetype"
+              className={styles.filterInput}
+              value={filterTerm}
+              onChange={(e) => {
+                setFilterTerm(e.target.value);
+              }}
+            />
+          </div>
           <button
             className={styles.clrBtn}
             onClick={() => {
