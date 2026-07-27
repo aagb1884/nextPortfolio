558282440f506d0badafc13317e33e37b0eae3f9
aagb1884 <a.a.g.blair@gmail.com>
2026-07-27 13:06:07 +0100

    simplify telepathic circuits

--- src/app/apps/randomiser/components/console/Right.tsx
+++ src/app/apps/randomiser/components/console/Right.tsx
@@ -1,4 +1,3 @@
-import { isIos, isAndroid, isIpadOS } from "@/app/functions/device";
 import styles from "@/app/styles/randomiser.module.css";
 
 interface rightProps {
@@ -22,29 +21,16 @@ const Right: React.FC<rightProps> = ({
   setSearchLink,
   setSearchTerm,
 }) => {
-  const mobile = isIos() || isAndroid() || isIpadOS();
-
   function telepathTime() {
     setTelepath(true);
     const randomWebLink = webLinks[Math.floor(Math.random() * webLinks.length)];
-    if (!mobile) {
-      setTelepath(true);
-      setTimeout(() => {
-        setTelepath(false);
-        window.open(
-          `https://www.ecosia.org/search?q=${randomWebLink}`,
-          "_blank"
-        );
-      }, 4000);
-    } else {
-      setTelepath(true);
-      setTimeout(() => {
-        setTelepath(false);
-        setSearchLink(true);
-      }, 2000);
-      setSearchLink(false);
-      setSearchTerm(randomWebLink);
-    }
+    setTelepath(true);
+    setTimeout(() => {
+      setTelepath(false);
+      setSearchLink(true);
+    }, 2000);
+    setSearchLink(false);
+    setSearchTerm(randomWebLink);
   }
 
   return (
